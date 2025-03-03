074b4b77fc8a2c5f44276f03b40026d40a70f7fa
QunshanWu <wuqunshan.2024@gmail.com>
2025-03-02 20:14:18 -0500

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,50 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = (cart) => {
+    let total = 0;
+
+    cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        
+        total += quantity * cost;
+    });
+
+    return total; 
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, amount: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, amount: item.quantity - 1 }));
+    } else {
+        dispatch(removeItem({ name: item.name }));
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const itemCost = parseFloat(item.cost.substring(1));
+    const subtotal = itemCost * item.quantity;  
+    return subtotal;
   };
 
   return (
